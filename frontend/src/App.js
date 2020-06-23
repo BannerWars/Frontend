bf4bd27e8de348bfb1376612d15ef3766e8a1a2d
Ariane Evans <ariane.evans@student.makeschool.com>
2020-06-22 19:08:47 -0700

    [add] routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,24 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
-// import Home from "./pages/Home"
-// import SignUp from "./pages/SignUp"
+import Home from "./pages/Home"
+import SignUp from "./pages/SignUp"
 import LogIn from "./pages/LogIn"
+import ForgotPassword from "./pages/ForgotPassword"
+import Verification from "./pages/Verification"
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <LogIn />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/log-in" component={LogIn} />
+          <Route exact path="/sign-up" component={SignUp} />
+          <Route exact path="/forgot-password" component={ForgotPassword} />
+          <Router exact path="verification" component={Verification} />
+        </Switch>
         <Footer />
       </div>
     </Router>
---
+++ frontend/src/pages/Verification.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Verification() {
+    return (
+        <div className="container">
+            <h1>Welcome to Banner Wars!</h1>
+            <p>Your account is now verified. Please log in.</p>
+            <a href="">Log in</a>
+        </div>
+    )
+}
