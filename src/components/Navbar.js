ffc294a3d5e06591ff607deac49cdb2a99b83b66
Ariane Evans <ariane.evans@student.makeschool.com>
2020-07-08 20:41:40 -0700

    [add] background grid navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,17 +13,20 @@ const navButtons = ["Rules", "Lobbies", "Log Out"]
 export default function Navbar() {
     return (
         <nav>
-            <div className="logo">
-                <a href="/">
-                    <h1>Banner Wars</h1>   
-                </a>             
+            <img src="./images/Grid.svg" alt="grid"/>
+            <div className="content">
+                <div className="logo">
+                    <a href="/">
+                        <h1>Banner Wars</h1>   
+                    </a>             
+                </div>
+                <ul className="nav_buttons">
+                    { 
+                        /* render nav buttons with links to corresponding pages */
+                        navButtons.map(text => <li key={text} ><Link className="nav-button" to={`/${text.toLowerCase()}`}>{text}</Link></li>)
+                    }
+                </ul>
             </div>
-            <ul className="nav_buttons">
-                { 
-                    /* render nav buttons with links to corresponding pages */
-                    navButtons.map(text => <li key={text} ><Link className="nav-button" to={`/${text.toLowerCase()}`}>{text}</Link></li>)
-                }
-            </ul>
         </nav>
     )
 }
